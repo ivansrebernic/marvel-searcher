eeefaf4f900afd95ae2b605e25894a0d45372e1b
Ivan Srebernic <srebernicivan@gmail.com>
2020-05-25 21:33:08 -0300

    added a few more unit tests

--- src/__test__/components/Characters.test.js
+++ src/__test__/components/Characters.test.js
@@ -5,17 +5,25 @@ import Characters from '../../containers/Characters'
 
 
 
-
+jest.mock('../../api')
 describe('<Characters/>', () => {
+    let wrapper;
     beforeAll(async () => {
         global.fetch = jest.fn()
-        API.fetchRandomCharacter = jest.fn();
-    })
-    it('Characters: renders correctly', async () => {
-        const wrapper = shallow(
+        API.fetchRandomCharacter.mockImplementation(() => { name: 'super hero name' })
+    });
+    beforeEach(() => {
+        wrapper = shallow(
             <Characters location={{ search: '' }}></Characters>
         );
+    })
+    it('Characters: renders correctly', async () => {
         expect(wrapper).toMatchSnapshot()
     });
-
+    it('fetches a random character on start (no url query)', () => {
+        setTimeout(() => {
+            wrapper.update()
+            expect(wrapper.state().results).toHaveLength(1)
+        })
+    });
 })
--- src/__test__/components/ComicList.test.js
+++ src/__test__/components/ComicList.test.js
@@ -4,8 +4,24 @@ import ComicList from '../../components/ComicList'
 
 
 describe('<ComicList>', () => {
+    let wrapper
+    beforeEach(() => {
+        wrapper = shallow(<ComicList></ComicList>)
+    })
     it('Renders correctly', () => {
-        const wrapper = shallow(<ComicList></ComicList>)
         expect(wrapper).toMatchSnapshot()
+    });
+    it('Fetches comics', () => {
+        const spy = jest.spyOn(global, 'fetch')
+        setTimeout(() => {
+            wrapper.update()
+            expect(spy).toHaveBeenCalledTimes(1)
+        })
+    });
+    it('displays a set of comics', () => {
+        const comics = [{ id: 1, title: "comic title 1" }, { id: 2, title: "comic title 2" }, { id: 3, title: "comic title 3" }]
+        wrapper.setState({ comics: comics });
+        console.log(wrapper.debug())
+        expect(wrapper.find('span')).toHaveLength(3)
     })
 })
